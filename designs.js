62c15be92792c6662a1ca242440e99521a6eb970
great-emeka <greatestgreatemeka@gmail.com>
2022-04-30 20:24:26 +0100

    worked on functionality of the makeGrid function

--- designs.js
+++ designs.js
@@ -8,11 +8,19 @@ const gridWidth = document.querySelector('#inputWidth').value;
 // Selecting the canvas grid
 const pixelCanvasGrid = document.querySelector('#pixelCanvas');
 
+// Create event listener functions that will be parsed as arguements
+function paintCell(event){
+    event.target.style.backGroundColor = inputColor;
+}
+
+// A function that will handle the grid making
+function makeGrid() {
+    // Resets inputs after each time submit button is clicked
+    pixelCanvasGrid.innerHTML = '';
+    // Loop through the input value for height and create equivalent rows
+    
+}
+
+
 
 // When size is submitted by the user, call makeGrid()
-
-function makeGrid() {
-
-// Your code goes here!
-
-}

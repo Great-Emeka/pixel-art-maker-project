2ef18e6531c70e433da732ad4554b623522d3d6b
great-emeka <greatestgreatemeka@gmail.com>
2022-04-30 20:07:28 +0100

    selected needed inputs and elements

--- designs.js
+++ designs.js
@@ -1,5 +1,13 @@
 // Select color input
+const inputColor = document.querySelector('#colorPicker').value;
+
 // Select size input
+const gridHeight = document.querySelector('#inputHeight').value;
+const gridWidth = document.querySelector('#inputWidth').value;
+
+// Selecting the canvas grid
+const pixelCanvasGrid = document.querySelector('#pixelCanvas');
+
 
 // When size is submitted by the user, call makeGrid()
 
